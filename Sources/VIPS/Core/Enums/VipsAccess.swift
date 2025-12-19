66611c2f9e79d79aa7a5d1f702df084dbf799c43
Tobias Haeberle <tobias.haeberle@me.com>
2025-12-19 16:42:55 +0100

    adds deprecations

--- Sources/VIPS/Core/Enums/VipsAccess.swift
+++ Sources/VIPS/Core/Enums/VipsAccess.swift
@@ -6,6 +6,11 @@ public typealias VipsAccess = Cvips.VipsAccess
 extension VipsAccess {
     public static var random: Self { VIPS_ACCESS_RANDOM }
     public static var sequential: Self { VIPS_ACCESS_SEQUENTIAL }
+    #if SHIM_VIPS_VERSION_8_18
+    @available(*, deprecated, renamed: "sequential")
     public static var sequentialUnbuffered: Self { VIPS_ACCESS_SEQUENTIAL_UNBUFFERED }
+    #else
+    public static var sequentialUnbuffered: Self { VIPS_ACCESS_SEQUENTIAL_UNBUFFERED }
+    #endif
     public static var last: Self { VIPS_ACCESS_LAST }
 }
--- Sources/VIPS/Core/Enums/VipsOperationFlags.swift
+++ Sources/VIPS/Core/Enums/VipsOperationFlags.swift
@@ -6,7 +6,12 @@ public typealias VipsOperationFlags = Cvips.VipsOperationFlags
 extension VipsOperationFlags {
     public static var none: Self { VIPS_OPERATION_NONE }
     public static var sequential: Self { VIPS_OPERATION_SEQUENTIAL }
+    #if SHIM_VIPS_VERSION_8_18
+    @available(*, deprecated, renamed: "sequential")
     public static var sequentialUnbuffered: Self { VIPS_OPERATION_SEQUENTIAL_UNBUFFERED }
+    #else
+    public static var sequentialUnbuffered: Self { VIPS_OPERATION_SEQUENTIAL_UNBUFFERED
+    #endif
     public static var nocache: Self { VIPS_OPERATION_NOCACHE }
     public static var deprecated: Self { VIPS_OPERATION_DEPRECATED }
     public static var untrusted: Self { VIPS_OPERATION_UNTRUSTED }
